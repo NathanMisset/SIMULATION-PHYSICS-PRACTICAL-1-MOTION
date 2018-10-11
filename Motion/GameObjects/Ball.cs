b604477c1a0338a9bd36777702afc8c72841e3f3
Nathan <nathan_misset@live.nl>
2018-10-11 11:02:00 +0200

    nm

--- Motion/GameObjects/Ball.cs
+++ Motion/GameObjects/Ball.cs
@@ -3,11 +3,12 @@ using Microsoft.Xna.Framework;
 
 namespace Motion {
     class Ball : SpriteGameObject {
-        protected float radius;
-        protected Vector2 acceleration;
-        private int nFrame = 0;
+        private float radius;
+        private Vector2 acceleration;
+        private float gravity;
+        private float friction;
 
-        public Ball(string assetName, Vector2 position, Vector2 velocity, float radius)
+        public Ball(string assetName, Vector2 position, Vector2 velocity, Vector2 acceleration, float gravity, float friction, float radius)
             : base(assetName, 0, "ball") {
             PerPixelCollisionDetection = false;
             Position = position;
@@ -15,51 +16,40 @@ namespace Motion {
             Origin = Center;
             this.radius = radius;
             scale = radius * 2 / Width;
+            this.acceleration = acceleration;
+            this.gravity = gravity;
+            this.friction = friction;
         }
 
         public override void Update(GameTime gameTime) {
-            switch (assetName)
-	        {
-            case "spr_ball_red":
-                if (nFrame % 60 == 0)
-                {
-                Velocity += acceleration;
-                }
-                if (position.X + radius >= Motion.Screen.X)
-                {
-                position.X = Motion.Screen.X;
-                velocity.X = -velocity.X;
-                acceleration.X = -acceleration.X;
-                }
-           
-                if (position.X - radius <= 0)
-                {
-                position.X = 0;
-                velocity.X = -velocity.X;
-                acceleration.X = -acceleration.X;
-                }
-                break;
-            case "PurpleSoftColorBall":
-                if (nFrame % 60 == 0 && velocity.Y != 0 || position.Y < Motion.Screen.Y - 30 )
-                {
-                velocity += acceleration;
-                }
-                if (position.Y + radius >= Motion.Screen.Y)
-                {
-                position.Y = Motion.Screen.Y;
-                velocity.Y -= 20;
-                velocity.Y = -velocity.Y;
-                }
-                nFrame++;
-                break;
-            case "PinkSoftColorBall":
-                    //niet gelukt
-                break;
-		        default:
-                break;
-	        }
-                                
+            Bounce();
+            velocity.Y += gravity;
+            velocity += acceleration;
+            velocity *= friction;
             base.Update(gameTime);
+        }
+        private void Bounce()
+        {
+            if (position.X + radius > Motion.Screen.X)
+            {
+                position.X = Motion.Screen.X - radius;
+                velocity.X = -velocity.X;
+            }
+            if (position.Y + radius > Motion.Screen.Y)
+            {
+                position.Y = Motion.Screen.Y - radius;
+                velocity.Y = -velocity.Y;
+            }
+            if (position.X - radius < 0)
+            {
+                position.X= radius;
+                velocity.X = -velocity.X;
+            }
+            if (position.Y - radius < 0)
+            {
+                position.Y = radius;
+                velocity.Y = -velocity.Y;
+            }
         }
     }
 }
--- Motion/GameStates/PlayingState.cs
+++ Motion/GameStates/PlayingState.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
-using Motion.GameObjects;
-using System;
 
-namespace Motion {
+namespace Motion
+{
     class PlayingState : GameObjectList {
         public PlayingState() {
             ///////////
@@ -15,15 +14,27 @@ namespace Motion {
             // step 1: Initialize three balls on the correct position
             Vector2 positionBallRed = new Vector2(30, Motion.Screen.Y / 2);
             Vector2 positionBallPink = new Vector2(30, Motion.Screen.Y - 30);
-            Vector2 positionBallPurple = new Vector2(Motion.Screen.Y / 2, 90);
+            Vector2 positionBallPurple = new Vector2(Motion.Screen.Y / 2, 30);
             // step 2: Give them the correct velocity
-            Vector2 velocityBallRed = new Vector2(1,0);
+            Vector2 velocityBallRed = Vector2.Zero;
             Vector2 velocityBallPink = new Vector2(150, -80.72f);
-            Vector2 velocityBallPurple = new Vector2(0, 10);
+            Vector2 velocityBallPurple = new Vector2(0, 100);
+            // acceleration
+            Vector2 accelerationRed = new Vector2(20, 0);
+            Vector2 accelerationPink = Vector2.Zero;
+            Vector2 accelerationPurple = Vector2.Zero;
+            // gravity
+            float gravityRed = 0;
+            float gravityPink = 0.5f;
+            float gravityPurple = 2;
+            // friction
+            float frictionRed = 1;
+            float frictionPink = 1;
+            float frictionPurple = 0.99f;
             // step 1: Initialize three balls on the correct position
-            var ballRed = new Ball("spr_ball_red", positionBallRed, velocityBallRed, 30f);
-            var ballPink = new Ball("PinkSoftColorBall", positionBallPink, velocityBallPink, 30f);
-            var ballPurple = new Ball("PurpleSoftColorBall", positionBallPurple, velocityBallPurple, 30f);
+            var ballRed = new Ball("spr_ball_red", positionBallRed, velocityBallRed, accelerationRed, gravityRed, frictionRed, 30f);
+            var ballPink = new Ball("PinkSoftColorBall", positionBallPink, velocityBallPink, accelerationPink, gravityPink, frictionPink, 30f);
+            var ballPurple = new Ball("PurpleSoftColorBall", positionBallPurple, velocityBallPurple, accelerationPurple, gravityPurple, frictionPurple, 30f);
             Add(ballRed);
             Add(ballPink);
             Add(ballPurple);
